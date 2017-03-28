476e96826df2aa8783fb8f238c6dda913406da15
m.battisti <m.battisti@almaviva.it>
2017-03-28 17:28:38 +0200

    secondo commit

--- app/src/main/java/android/battistim/it/busfinder/MainActivity.java
+++ app/src/main/java/android/battistim/it/busfinder/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //add a test comment
        // txtView = (TextView) this.findViewById(R.id.textView);
 
 
